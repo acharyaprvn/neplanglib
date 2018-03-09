ac85a4a6a810c9d861d3520e6dbe56b15fd2eaad
Praveen Acharya <acharyaprvn@gmail.com>
2018-03-09 12:22:02 +0545

    functions for adding and removing punctuations

--- tokenize.py
+++ tokenize.py
@@ -1,18 +1,49 @@
 import sys, codecs, string, re
 
+punctList = [x for x in string.punctuation]
+
+def addPunct():
+	"""
+	A function to add additional punctuations other than standard English punctuations provided by string.punctuation
+	"""
+	newPunctList = ['|']
+	for x in newPunctList:
+		punctList.append(x)
+
+
+def removePunct():
+	"""
+	A function to remove unnecessary punctuations provided by string.punctuation
+	"""
+	delPunctList = ['.',':','-']
+	for x in delPunctList:
+		punctList.remove(x)
+
+
+def punctListToString():
+	"""
+	A function that adds, removes and converts the puncuations List into a string
+
+	returns: a string
+	"""
+	addPunct()
+	removePunct()
+	punctString = ''.join(punctList)	
+	return punctString
 
 def tokenize(line):
 	"""
+	A tokenzer that tokenizes in punctuation boundaries for Nepali language
 
+	returns: a list of tokens
 	"""
-	punctuations = string.punctuation.replace('.','')
-	pattern = r'([' + re.escape(punctuations) + u"\u0964" + r'])'
+	
+	punct = punctListToString()
+	pattern = r'([' + re.escape(punct) + r'])'
 	tok_pattern =  re.compile(pattern)
 	tok_line = tok_pattern.sub(r' \1 ', line)
-
 	#replace tab and additional spaces
 	processed_line = re.sub(r'[ \t]+', ' ', tok_line)
-	#print(tok_str)
 	return processed_line.strip(' ').split(' ')
 	
 
